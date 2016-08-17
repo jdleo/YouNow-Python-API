508aaf1a1b0f6ae33e138965a147a8be6245a04a
John Leonardo <jdleonardo5@icloud.com>
2016-08-17 13:10:43 -0700

    oops, should have returned data

--- YouNow.py
+++ YouNow.py
@@ -12,11 +12,11 @@ class YouNow:
         response = requests.get(getTrending)
         response.raise_for_status()
         data = response.json()['trending_tags']
-        print(data)
+        return data
 
     def getTrendingUsers(self):
         getTrending = "https://cdn.younow.com/php/api/younow/dashboard/locale=en/trending=50"
         response = requests.get(getTrending)
         response.raise_for_status()
         data = response.json()['trending_users']
-        print(data)
+        return data
