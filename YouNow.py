822b3073e637ab014c019e603c62bd2891a8ff49
John Leonardo <jdleonardo5@icloud.com>
2016-08-17 13:54:29 -0700

    found better url parameter

--- YouNow.py
+++ YouNow.py
@@ -14,9 +14,10 @@ class YouNow:
         data = response.json()['trending_tags']
         return data
 
-    def getTrendingUsers(self):
-        getTrending = "https://cdn.younow.com/php/api/younow/dashboard/locale=en/trending=50"
+    def getTrendingUsers(self, startFrom):
+        getTrending = "https://api.younow.com/php/api/younow/trendingUsers/locale=en/numberOfRecords=50/startFrom=%s" % startFrom
         response = requests.get(getTrending)
         response.raise_for_status()
-        data = response.json()['trending_users']
+        data = response.json()
         return data
+
